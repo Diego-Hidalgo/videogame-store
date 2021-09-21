96eb407144a5d6a106f66f69dcf32b0932706609
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-20 20:54:55 -0500

    Added comments on VideoGame class

--- src/model/VideoGame.java
+++ src/model/VideoGame.java
@@ -16,34 +16,34 @@ public class VideoGame {
 
 	public int getCode() {
 		return code;
-	}
+	}//End getCode
 
 	public void setCode(int code) {
 		this.code = code;
-	}
+	}//End setCode
 
 	public int getQuantity() {
 		return quantity;
-	}
+	}//End getQuantity
 
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
-	}
+	}//End setQuantity
 
 	public String getShelf() {
 		return shelf;
-	}
+	}//End getShelf
 
 	public void setShelf(String shelf) {
 		this.shelf = shelf;
-	}
+	}//End setShelf
 
 	public double getPrice() {
 		return price;
-	}
+	}//End getPrice
 
 	public void setPrice(double price) {
 		this.price = price;
-	}
+	}//End setPrice
 	
 }//End VideoGame class
