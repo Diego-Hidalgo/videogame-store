73e7ba5787c9389d55449c963285312725f3a85b
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-20 20:49:38 -0500

    Added getters and setters to Shelf class

--- src/model/Shelf.java
+++ src/model/Shelf.java
@@ -4,4 +4,16 @@ public class Shelf {
 	
 	private String identifier;
 
-}
+	public Shelf(String identifier) {
+		this.identifier = identifier;
+	}//End Constructor
+
+	public String getIdentifier() {
+		return identifier;
+	}//End getIdentifier
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}//End setIdentifier
+
+}//End shelf class
