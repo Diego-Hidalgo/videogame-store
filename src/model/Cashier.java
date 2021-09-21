0bff89da93830e46cdff190a78212faea6914dcd
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-20 20:41:14 -0500

    Added getters and setters to Cashier class

--- src/model/Cashier.java
+++ src/model/Cashier.java
@@ -3,5 +3,17 @@ package model;
 public class Cashier {
 
 	private boolean inUse;
+
+	public Cashier(boolean inUse) {
+		this.inUse = inUse;
+	}//End Constructor
+
+	public boolean getInUse() {
+		return inUse;
+	}//End getInUse
+
+	public void setInUse(boolean inUse) {
+		this.inUse = inUse;
+	}//End setInUse
 	
-}
+}//End Cashier class
