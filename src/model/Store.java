643f72923b9144d4bd3c3ea8740dcf2c4ec9e0b2
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-20 21:00:50 -0500

    Little changes to Store class

--- src/model/Store.java
+++ src/model/Store.java
@@ -1,5 +1,21 @@
 package model;
 
+import dataStructures.queue.Queue;
+
 public class Store {
 
-}
+    private Queue<Client> clients;
+
+    public Store() {
+        clients = new Queue<>();
+    }//End Store
+
+    public Queue<Client> getClients() {
+        return clients;
+    }//End getClients
+
+    public void setClients(Queue<Client> clients) {
+        this.clients = clients;
+    }//End setClients
+
+}//End Store class
