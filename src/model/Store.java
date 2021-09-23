70b869be064cba02a935fce49f0a6dc4fbc92487
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-23 11:29:12 -0500

    Little changes in Store class

--- src/model/Store.java
+++ src/model/Store.java
@@ -1,21 +1,18 @@
 package model;
 
+import dataStructures.linkedlist.LinkedList;
 import dataStructures.queue.Queue;
 
 public class Store {
 
     private Queue<Client> clients;
+    private LinkedList<Cashier> cashiers;
+    private LinkedList<Shelf> shelves;
 
     public Store() {
         clients = new Queue<>();
     }//End Store
 
-    public Queue<Client> getClients() {
-        return clients;
-    }//End getClients
 
-    public void setClients(Queue<Client> clients) {
-        this.clients = clients;
-    }//End setClients
 
 }//End Store class
