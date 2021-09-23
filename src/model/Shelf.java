0528bf7e77fd60e23669b173f0b762950290d498
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-09-23 11:26:14 -0500

    Added methods to add, remove, and search a videogame in a shelf

--- src/model/Shelf.java
+++ src/model/Shelf.java
@@ -1,11 +1,16 @@
 package model;
 
+import dataStructures.hashtable.HashTable;
+import dataStructures.hashtable.HashTableException;
+
 public class Shelf {
 	
 	private String identifier;
+	private HashTable<Integer, VideoGame> videoGames;
 
 	public Shelf(String identifier) {
 		this.identifier = identifier;
+		videoGames = new HashTable<>();
 	}//End Constructor
 
 	public String getIdentifier() {
@@ -15,5 +20,25 @@ public class Shelf {
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}//End setIdentifier
+
+	public HashTable<Integer, VideoGame> getVideoGames() {
+		return videoGames;
+	}//End getVideoGames
+
+	public void setVideoGames(HashTable<Integer, VideoGame> videoGames) {
+		this.videoGames = videoGames;
+	}//End setVideoGames
+
+	public void addGame(VideoGame toAdd) throws HashTableException {
+		videoGames.insert(toAdd.getCode(), toAdd);
+	}//End addGame
+
+	public VideoGame searchVideoGame(int code) {
+		return videoGames.search(code);
+	}//End searchVideoGame
+
+	public boolean removeVideoGame(int code) {
+		return videoGames.remove(code);
+	}//End removeVideoGames
 
 }//End shelf class
