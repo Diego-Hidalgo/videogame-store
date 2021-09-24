d1751aa4c1dd2aaefa57f1ed22be5d645bc03bcd
Diego-Hidalgo <d13g01hl@gmail.com>
2021-09-24 07:39:15 -0500

    Added a Client attribute and related methods in Cashier class

--- src/model/Cashier.java
+++ src/model/Cashier.java
@@ -2,11 +2,27 @@ package model;
 
 public class Cashier {
 
+	private Client current;
 	private boolean inUse;
 
 	public Cashier(boolean inUse) {
 		this.inUse = inUse;
+		current = null;
 	}//End Constructor
+
+	public Client getCurrent() {
+		return current;
+	}//End getCurrent
+
+	public void setCurrent(Client current) {
+		this.current = current;
+		inUse = true;
+	}//End setCurrent
+
+	public void removeCurrent() {
+		current = null;
+		inUse = false;
+	}//End removeCurrent
 
 	public boolean getInUse() {
 		return inUse;
