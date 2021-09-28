65f0000822eb4b4aac6c1edfa0ff9d240acca623
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-09-28 16:23:34 -0500

    Implemented tests for the data structures

--- tests/dataStructures/QueueTest.java
+++ tests/dataStructures/QueueTest.java
@@ -1,5 +1,28 @@
 package dataStructures;
 
-public class QueueTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import dataStructures.hashtable.HashTable;
+import dataStructures.hashtable.HashTableException;
+import dataStructures.queue.Queue;
+import model.Client;
+import model.VideoGame;
+
+public class QueueTest {
+	public Queue<Client> setupScenary1(){
+		Queue<Client> queue = new Queue<>();
+		return queue;
+	}
+	
+	@Test
+	public void testInsert() throws HashTableException {
+		Queue<Client> queue = setupScenary1();
+		
+		Client client = new Client(null);
+		
+		//assertEquals();
+	}
+	
 }
